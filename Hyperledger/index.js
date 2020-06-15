2a6d905f72717b05fae33ccc894134ca840d9328
kimiaHonari <63830882+kimiaHonari@users.noreply.github.com>
2020-06-15 01:27:11 -0600

    Add files via upload

---
+++ Hyperledger/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const ContractContract = require('./lib/contract-contract');
+
+module.exports.ContractContract = ContractContract;
+module.exports.contracts = [ ContractContract ];
